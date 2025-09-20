34f9a3c99bf6364e5664df4099f85c9c5005dc6d
torgomyan01 <and.torgomyan01@gmail.com>
2025-09-20 18:03:01 +0400

    sss

--- ws-server.js
+++ ws-server.js
@@ -1,34 +1,34 @@
 import { PrismaClient } from "@prisma/client";
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
+
 const prisma = new PrismaClient();
 
 const wss = new WebSocketServer({ port: 3004 });
 
-// eslint-disable-next-line no-undef
 console.log("WebSocket server is running on port 3004");
 
 wss.on("connection", (ws) => {
-  // eslint-disable-next-line no-undef
   console.log("Client connected");
 
   ws.on("message", async (message) => {
     try {
       const data = JSON.parse(message);
-      if (data.type === "NEW_MESSAGE") {
+
+      // Ստուգել, որ հաղորդագրությունը ունի բոլոր պարտադիր դաշտերը
+      if (data.type === "NEW_MESSAGE" && data.sender_id && data.content) {
         const newMessage = await prisma.messages.create({
           data: {
             chat_id: data.chat_id,
             sender_id: data.sender_id,
             content: data.content,
-            file_type: data.file_type,
-            file_paths: data.file_paths,
-            selected_chat_id: data.selected_chat_id,
+            file_type: data.file_type || null, // Կամայական դաշտեր
+            file_paths: data.file_paths || null,
+            selected_chat_id: data.selected_chat_id || null,
             created_at: new Date(),
           },
         });
 
         wss.clients.forEach((client) => {
-          // eslint-disable-next-line no-undef
           if (client.readyState === WebSocket.OPEN) {
             client.send(
               JSON.stringify({ type: "MESSAGE", payload: newMessage }),
@@ -37,18 +37,15 @@ wss.on("connection", (ws) => {
         });
       }
     } catch (error) {
-      // eslint-disable-next-line no-undef
       console.error("Error handling message:", error);
     }
   });
 
   ws.on("close", () => {
-    // eslint-disable-next-line no-undef
     console.log("Client disconnected");
   });
 
   ws.on("error", (error) => {
-    // eslint-disable-next-line no-undef
     console.error("WebSocket error:", error);
   });
 });
